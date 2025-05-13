f84365220deb5192ce46282624328fe3c5d4bc8d
Eduardo Eugen Boieru <ediboieru@gmail.com>
2025-05-13 17:53:16 +0200

    update waybar and wlogout

--- waybar/.config/waybar/scripts/power-menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chosen=$(printf "󰍃 Logout\n󰜉 Reboot\n Shutdown" | wofi --dmenu --width 200 --height 150 --prompt "Power")
-
-case "$chosen" in
-  "󰍃 Logout") hyprctl dispatch exit ;;
-  "󰜉 Reboot") systemctl reboot ;;
-  " Shutdown") systemctl poweroff ;;
-esac
